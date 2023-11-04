94f239c790dd58d06efc4927e8a1c8cc54aef7c4
oussamaym <oussamaelysm15@gmail.com>
2023-11-04 21:38:54 +0100

    Création du template

---
+++ src/main/java/org/emsi/tp2oussama/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package org.emsi.tp2oussama;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/org/emsi/tp2oussama/jsf/Bean2.java
@@ -0,0 +1,37 @@
+package org.emsi.tp2oussama.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+
+/**
+ * Backing bean pour champs Nombre du formulaire 4*
+ */
+@Named(value = "bean2")
+@RequestScoped
+public class Bean2 {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    public String afficher() {
+        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+}
