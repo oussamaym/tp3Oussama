dc6a82ae5b95b77c5bf99677c2834f581abd2f77
oussamaym <oussamaelysm15@gmail.com>
2023-11-04 23:20:14 +0100

    Création des clients du template et Paramétrage du template

--- src/main/java/org/emsi/tp3oussama/jsf/Bean2.java
+++ src/main/java/org/emsi/tp3oussama/jsf/Bean2.java
@@ -32,6 +32,6 @@ public class Bean2 {
     }
 
     public String afficher() {
-        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+        return "affichage__4?nb=" + nombre + "&amp;faces-redirect=true";
     }
 }
